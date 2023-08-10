05be94088e2709dad77e8a42dd805a2e060c6fdc
Mitch <mitchelldmccollum@gmail.com>
2023-08-10 18:23:34 -0400

    init commit

--- player.gd
+++ player.gd
@@ -9,40 +9,31 @@ var syncPos = Vector2(0,0)
 var syncRot = 0
 @export var bullet :PackedScene
 
-func _ready():
-	$MultiplayerSynchronizer.set_multiplayer_authority(str(name).to_int())
-
 func _physics_process(delta):
-	if $MultiplayerSynchronizer.get_multiplayer_authority() == multiplayer.get_unique_id():
-		# Add the gravity.
-		if not is_on_floor():
-			velocity.y += gravity * delta
-			
-		$GunRotation.look_at(get_viewport().get_mouse_position())
-		# Handle Jump.
-		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-			velocity.y = JUMP_VELOCITY
+
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
 		
-		syncPos = global_position
-		syncRot = rotation_degrees
-		if Input.is_action_just_pressed("Fire"):
-			fire.rpc()
-		# Get the input direction and handle the movement/deceleration.
-		# As good practice, you should replace UI actions with custom gameplay actions.
-		var direction = Input.get_axis("ui_left", "ui_right")
-		if direction:
-			velocity.x = direction * SPEED
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
+	$GunRotation.look_at(get_viewport().get_mouse_position())
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	syncPos = global_position
+	syncRot = rotation_degrees
+	if Input.is_action_just_pressed("Fire"):
+		var b = bullet.instantiate()
+		b.global_position = $GunRotation/BulletSpawn.global_position
+		b.rotation_degrees = $GunRotation.rotation_degrees
+		get_tree().root.add_child(b)
 
-		move_and_slide()
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
 	else:
-		global_position = global_position.lerp(syncPos, .5)
-		rotation_degrees = lerpf(rotation_degrees, syncRot, .5)
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-@rpc("any_peer","call_local")
-func fire():
-	var b = bullet.instantiate()
-	b.global_position = $GunRotation/BulletSpawn.global_position
-	b.rotation_degrees = $GunRotation.rotation_degrees
-	get_tree().root.add_child(b)
+	move_and_slide()
